b82ec8b7059025be58587422d0ef7acd62facdab
peelsannaw <2736522552@qq.com>
2023-06-26 14:39:33 +0800

    init commit

---
+++ backend/enviroment_backend/web/src/main/java/com/peels/controller/AqiFeedbackController.java
@@ -0,0 +1,21 @@
+package com.peels.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * <p>
+ *  前端控制器
+ * </p>
+ *
+ * @author peelsannaw
+ * @since 2023-06-26
+ */
+@Controller
+@RequestMapping("/aqiFeedback")
+public class AqiFeedbackController {
+
+}
+
